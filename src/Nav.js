480baedc5d4c79c209a6a294b6a3ffdb4a970854
Danushram Vijayakuamr <danushramv12@gmail.com>
2021-07-05 12:08:51 +0530

    deploy netflix clone

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Nav.css";
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img className="nav_logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                alt="netflix-logo" />
+            <img className="nav_profile"
+                src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png"
+                alt="netflix-profile" />
+            
+        </div>
+    )
+}
+
+export default Nav
